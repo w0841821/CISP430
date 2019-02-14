3944eb3f2c21019df43dffaedbe017382b0e6bd5
EA <errollabrahamian@gmail.com>
2019-02-14 04:08:13 -0800

    0214 04:08

---
+++ HW2/HW2.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+class ListTest
+{
+private:
+  int* dynArray = nullptr;
+  int oldSize, newSize;
+public:
+  void newArray();
+  void incArray();
+  void delArray();
+  void populate();
+  void find();
+  void add();
+  void size();
+  void print();
+};
+
+template <class T>
+T NewIncPopDel (T size)
+{
+  ListTest.newArray();
+  ListTest.incArray(size);
+  ListTest.populate();
+  ListTest.delArray();
+}
+
+int main()
+{
+  ListTest hw2;
+
+  const int oneK = 1000;
+  const int tenK = 10000;
+  const int hunK = 100000;
+	const int oneM = 1000000;
+	const int tenM = 10000000;
+
+  srand(time(0));
+
+  //hw2.newArray();
+  NewIncPopDel<int oneK>;
+  hw2.populate();
+  hw2.find();
+  hw2.add();
+  hw2.size();
+  hw2.print();
+}
+
+void ListTest::newArray()
+{
+  oldSize = 10;
+  newSize = oldSize;
+  dynArray = new int[10];
+}
+
+void ListTest::incArray()
+{
+  newSize *= 2;
+
+  int* tmpArr = nullptr;
+  tmpArr = new int[newSize];
+
+  for (int i = 0; i < oldSize; i++)
+    tmpArr[i] = dynArray[i];
+
+  delete [] dynArray;
+
+  dynArray = tmpArr;
+
+  tmpArr = nullptr;
+}
+
+void ListTest::delArray()
+{
+  delete [] dynArray;
+}
+
+void ListTest::populate()
+{
+  for (int i = oldSize; i < newSize; i++)
+    dynArray[i] = (rand() % 1000);
+  oldSize = newSize;
+}
+
+void ListTest::find()
+{
+  int x = (rand() % 1000);
+  int i;
+  bool found = false;
+
+  cout << "Searching for: " << x << endl;
+
+  auto start = chrono::high_resolution_clock::now();
+
+  for (i = 0; i < newSize; i++)
+  {
+    if (x == dynArray[i])
+    {
+      found = true;
+      break;
+    }
+  }
+
+  auto stop = chrono::high_resolution_clock::now();
+
+  if (found)
+    cout << "Random number " << x << " was found at element " << i << ".\n";
+  if (!found)
+    cout << "Random number " << x << " was not found.\n";
+
+  auto timing = stop - start;
+
+  cout << "\nThe search took " << chrono::duration_cast<chrono::microseconds>(timing).count() << " microseconds\n";
+}
+
+void ListTest::add()
+{
+
+}
+
+void ListTest::size()
+{
+
+}
+
+void ListTest::print()
+{
+
+}
